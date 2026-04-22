f1b381ca1a8163982c63a29a86774926e7f92644
codemathphp <codemathphp@gmail.com>
2026-04-22 03:35:01 +0200

    fix: remove duplicate JSX code in ClientLayout
    - Remove duplicated if (!mounted) and return blocks at end of file
    - Fixes syntax error that was causing build failure on Vercel

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -33,18 +33,3 @@ export default function ClientLayout({ children }) {
     </div>
   )
 }
-
-  if (!mounted) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin">Loading...</div>
-      </div>
-    )
-  }
-
-  return (
-    <div className="min-h-screen max-w-md mx-auto bg-white relative">
-      {children}
-    </div>
-  )
-}
